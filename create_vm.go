1650e58108351562a445dca24e152536468d6631
Iradukunda Jean de Dieu <49599811+iradukunda1@users.noreply.github.com>
2023-06-22 14:45:23 +0200

    refactor cloud-config file syntax (#11)
    * refactor the process of start and creating vm machine using go key word
    
    * include machine state and made some refactor
    - this state machine are not regit working as expected.
    
    * refactor cloud-config file syntax

--- create_vm.go
+++ create_vm.go
@@ -9,41 +9,25 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-var duration = 60 * time.Second
+var duration = 120 * time.Second
 
 // CreateVmm is responsible to create vm and return its ip address
 func (o *options) createVMM(ctx context.Context, id string) (*Firecracker, error) {
 
-	vmCtx, _ := context.WithTimeout(ctx, duration)
-	// defer vmCancel()
-
 	llg := log.New()
 
-	fcCfg := o.getConfig()
-
-	// if fcCfg.JailerCfg == nil {
-	// _ = firecracker.VMCommandBuilder{}.
-	// 	WithBin(o.FcBinary).
-	// 	WithSocketPath(fcCfg.SocketPath).
-	// 	WithArgs([]string{"--id", id}).
-	// 	WithStdin(os.Stdin).
-	// 	WithStdout(os.Stdout).
-	// 	WithStderr(os.Stderr).
-	// 	Build(ctx)
-	// }
+	cfg := o.getConfig()
 
 	// client := firecracker.NewClient(fcCfg.SocketPath, llg.WithContext(vmmCtx), true)
 
 	machineOpts := []firecracker.Opt{
-		// firecracker.WithClient(client),
-		// firecracker.WithProcessRunner(cmd),
-		// firecracker.WithProcessRunner(o.jailerCommand(vmmCtx, id, true)),
 		firecracker.WithLogger(log.NewEntry(llg)),
 	}
 
-	fcCfg.VMID = id
-	fcCfg.JailerCfg.ID = id
-	if err := exposeBlockDeviceToJail(o.RootFsImage, *fcCfg.JailerCfg.UID, *fcCfg.JailerCfg.GID); err != nil {
+	cfg.VMID = id
+	cfg.JailerCfg.ID = id
+
+	if err := exposeBlockDeviceToJail(o.RootFsImage, *cfg.JailerCfg.UID, *cfg.JailerCfg.GID); err != nil {
 		return nil, fmt.Errorf("failed to expose fs to jail: %v", err)
 	}
 
@@ -56,48 +40,17 @@ func (o *options) createVMM(ctx context.Context, id string) (*Firecracker, error
 		return nil, fmt.Errorf("failed to set network: %s", err)
 	}
 
-	// g := errgroup.Group{}
-
-	// out := make(chan *firecracker.Machine)
-
-	// g.Go(func() error {
-
-	m, err := firecracker.NewMachine(vmCtx, fcCfg, machineOpts...)
+	m, err := firecracker.NewMachine(ctx, cfg, machineOpts...)
 	if err != nil {
 		return nil, fmt.Errorf("failed creating machine: %v", err)
 	}
 
-	// 	if err = machine.Start(vmCtx); err != nil {
-	// 		return fmt.Errorf("failed to start machine: %v", err)
-	// 	}
-
-	// 	out <- machine
-
-	// 	return nil
-	// })
-
-	// m := <-out
-
-	// defer m.StopVMM()
-
-	// if g.Wait() != nil {
-	// 	log.Fatalf("error during start and create vm %v", g.Wait())
-	// }
-
-	// go func() {
-	// 	m.Wait(ctx)
-	// }()
-
-	installSignalHandlers(vmCtx, m)
-
-	// if err := m.Wait(ctx); err != nil {
-	// 	log.Fatalf("failed to start machine during awaiting for vm health: %v", err)
-	// }
+	installSignalHandlers(ctx, m)
 
 	res := &Firecracker{
-		ctx:  vmCtx,
+		ctx:  ctx,
 		Name: o.ProvidedImage,
-		// cancelCtx: vmCancel,
+		// cancelCtx: nil,
 		vm:    m,
 		state: StateCreated,
 	}
